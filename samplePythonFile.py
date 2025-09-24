233b179c80cc0a4612f5fbee2fc52b31e3061f66
jbashburn <jbashburn42@tntech.edu>
2025-09-23 19:30:58 -0500

    commit message for hello world

--- samplePythonFile.py
+++ samplePythonFile.py
@@ -1,1 +1,8 @@
 print("hello, world")
+
+# I have staged this comment but not committed!!!
+
+#here is another comment. 
+
+#we can delete this secction of comments and
+#return back to our staged changes
